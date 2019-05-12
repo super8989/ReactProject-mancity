e9ade16493d864ae4693797a73ae0173634957eb
Sam <super8989@gmail.com>
2019-05-12 16:07:31 -0400

    finishing header and adding footer

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -5,6 +5,8 @@ import Button from "@material-ui/core/Button";
 
 import { Link } from "react-router-dom";
 
+import { CityLogo } from "../ui/icons";
+
 class Header extends Component {
   render() {
     return (
@@ -17,9 +19,11 @@ class Header extends Component {
           borderBottom: "2px solid #00285e"
         }}
       >
-        <Toolbar styles={{ display: "flex" }}>
-          <div styles={{ flexGrow: 1 }}>
-            <div className="header_logo">Logo</div>
+        <Toolbar style={{ display: "flex" }}>
+          <div style={{ flexGrow: 1 }}>
+            <div className="header_logo">
+              <CityLogo link={true} linkTo="/" width="70px" height="70px" />
+            </div>
           </div>
 
           <Link to="/the_team">
