34e60833004463200fd6bcf6949bbe28410accd9
Sam <super8989@gmail.com>
2019-05-12 15:39:24 -0400

    adding header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Resources/css/app.css';
 
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
