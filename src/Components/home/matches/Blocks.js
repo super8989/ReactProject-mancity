985a9b58901f389af885a6b589338d1dc4748f1a
Sam <super8989@gmail.com>
2019-06-02 22:36:03 -0400

    home: finishing the match blocks component

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { firebaseMatches } from "../../../firebase";
-import { firebaseLooper, reverseArray } from '../../ui/misc';
+import { firebaseLooper, reverseArray } from "../../ui/misc";
+
+import MatchesBlock from "../../ui/matches_block";
+import Slide from "react-reveal/Slide";
 
 class Blocks extends Component {
   state = {
@@ -8,23 +11,33 @@ class Blocks extends Component {
   };
 
   componentDidMount() {
-    firebaseMatches.limitToLast(6).once('value').then((snapshot)=> {
-      const matches = firebaseLooper(snapshot);
+    firebaseMatches
+      .limitToLast(6)
+      .once("value")
+      .then(snapshot => {
+        const matches = firebaseLooper(snapshot);
 
-      this.setState({
-        matches: reverseArray(matches)
-      })
-    })
+        this.setState({
+          matches: reverseArray(matches)
+        });
+      });
   }
 
-  showMatches = () => (
-    <div>
-      match
-    </div>
-  )
+  showMatches = matches =>
+    matches
+      ? matches.map(match => (
+          <Slide bottom key={match.id}>
+            <div className="item">
+              <div className="wrapper">
+                <MatchesBlock match={match} />
+              </div>
+            </div>
+          </Slide>
+        ))
+      : null;
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <div className="home_matches">{this.showMatches(this.state.matches)}</div>
     );
