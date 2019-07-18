24c2ce9de01faa75220282458c0e9b98df648a09
Sam <super8989@gmail.com>
2019-07-17 21:58:32 -0400

    Home: Enroll and form fields 2

--- src/Components/home/promotion/Enroll.js
+++ src/Components/home/promotion/Enroll.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import FormField from '../../ui/formFields';
+import { validate } from '../../ui/misc';
 
 class Enroll extends Component {
 
@@ -26,6 +27,26 @@ class Enroll extends Component {
     }
   }
 
+  updateForm(element) {
+    const newFormdata =  {...this.state.formdata}
+    const newElement = { ...newFormdata[element.id] }
+
+    newElement.value = element.event.target.value;
+
+    let validData = validate(newElement)
+
+    console.log(validData);
+
+
+
+    newFormdata[element.id] = newElement
+
+    this.setState({
+      formdata: newFormdata
+    })
+
+  }
+
   submitForm() {
 
   }
@@ -43,7 +64,7 @@ class Enroll extends Component {
               <FormField
                 id={'email'}
                 formdata={this.state.formdata.email}
-                
+                change={(element)=> this.updateForm(element)}
 
               />
 
