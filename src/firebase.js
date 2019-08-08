aa8f4e853f8866cc47cf216e3c1afa7cbab3f655
Sam <super8989@gmail.com>
2019-08-07 22:31:59 -0400

    Admin: Players fileuploader continued

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyD6---0nA5yWrZsyI52EFZck84g4oZHo00",
